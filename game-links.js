ab32b36b7904630b3f5d9379a84093b5f82d7087
chelinomx <107780203+chelinomx@users.noreply.github.com>
2023-10-21 12:52:56 -0500

    Update and rename games.json to game-links.js

---
+++ game-links.js
@@ -0,0 +1,10 @@
+// game-links.js
+const gameLinks = [
+  {
+    name: 'Another Game',
+    description: 'Description of Another Game.',
+    image: './images/another-game.jpg',
+    playFunction: 'play("Another Game", "./game/another-game")',
+  },
+  // Add more game links as needed
+];
